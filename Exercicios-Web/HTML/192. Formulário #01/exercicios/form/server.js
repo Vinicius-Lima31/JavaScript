3e3d6cbd4d2e4e1a0d6b050ecd8033d910a9d8e2
Vinícius Lima Da Silva <Vinicius-Lima31@github.com>
2020-07-13 17:08:17 -0300

    209, 210, 211, 212

---
+++ Exercicios-Web/HTML/192. Formulário #01/exercicios/form/server.js
@@ -0,0 +1,31 @@
+// Servidor Criado com "Express"
+// Precisamos do "Express" e "body-parser"
+
+const express = require("express")
+const app = express()
+
+/*  O que o "body-parser" vai fazer é, uma vez que submeto meus dados eu um formulario
+    ele vai fazer um parser no corpo da requisição, vai pegar esses dados e jogare dentro
+    do request.body, ou seja, quando eu acessar request.body, eu vou ter acesso a todos os dados do formulario que submeti
+*/
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// no "form.html" o método é post, então usaremos post, e o endereço é /usuario
+app.post("/usuarios", (req, resp, next) => // Quero atender uma requisição do tipo "post"
+{ // Passo uma Middleware
+    console.log(req.body) // Para saber de fato oque chegou no body da requisição
+    resp.send("<h1>Parabéns</h1>") // Quando o úsuario clicar no formulario para submeter.
+}) 
+
+app.listen(3003, ()=>
+{
+    console.log(`Executando na Porta ${3003}`)
+})
+
+// Se não tivesse o body-parser não iria funcionar, porque não teria como ele ir fazer um parser no corpo da requisição
+
+// Executando o html
+// http-server -c-1
+// Executando com memoria cache desativada
---
+++ Exercicios-Web/HTML/197. Desafio Slider #02/exercicios/form/server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+
+/*  O que o "body-parser" vai fazer é, uma vez que submeto meus dados eu um formulario
+    ele vai fazer um parser no corpo da requisição, vai pegar esses dados e jogare dentro
+    do request.body, ou seja, quando eu acessar request.body, eu vou ter acesso a todos os dados do formulario que submeti
+*/
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// no "form.html" o método é post, então usaremos post, e o endereço é /usuario
+app.post("/usuarios", (req, resp, next) => // Quero atender uma requisição do tipo "post"
+{ 
+    console.log(req.body) // Para saber de fato oque chegou no body da requisição
+    resp.send("<h1>Parabéns. Usuario Incluido </h1>") // Quando o úsuario clicar no formulario para submeter.
+}) 
+
+// Aula 185
+
+app.post("/usuarios/:id", (req, resp, next) => // Vou passar um id para ser alterado
+{ 
+    console.log(req.params.id) // quero que ele mostre o "id"
+    console.log(req.body) // Mostre o que foi adcionado no formulario 
+    resp.send("<h1>Parabéns. Usuario Alterado </h1>") 
+}) 
+
+app.listen(3003, ()=>
+{
+    console.log(`Executando na Porta ${3003}`)
+})
+
+// Se não tivesse o body-parser não iria funcionar, porque não teria como ele ir fazer um parser no corpo da requisição
